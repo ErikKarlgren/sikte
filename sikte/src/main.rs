088bdf282060ffec8d51171422aa609eeff2a388
Erik Karlgren Domercq <erik.karldom@gmail.com>
2025-08-16 01:27:37 +0200

    refactor(main): trace read calls instead of perf events

--- sikte/src/main.rs
+++ sikte/src/main.rs
@@ -1,7 +1,7 @@
-use aya::{
-    programs::{PerfEvent, perf_event},
-    util::online_cpus,
-};
+mod programs;
+use aya::programs::TracePoint;
+use programs::{get_tracepoints_program, load_ebpf_object};
+
 #[rustfmt::skip]
 use log::{debug, warn};
 use tokio::signal;
@@ -21,31 +21,31 @@ async fn main() -> anyhow::Result<()> {
         debug!("remove limit on locked memory failed, ret is: {ret}");
     }
 
-    // This will include your eBPF object file as raw bytes at compile-time and load it at
-    // runtime. This approach is recommended for most real-world use cases. If you would
-    // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Bpf::load_file` instead.
-    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
-        env!("OUT_DIR"),
-        "/sikte"
-    )))?;
+    let mut ebpf = load_ebpf_object()?;
+
     if let Err(e) = aya_log::EbpfLogger::init(&mut ebpf) {
         // This can happen if you remove all log statements from your eBPF program.
         warn!("failed to initialize eBPF logger: {e}");
     }
-    // This will raise scheduled events on each CPU at 1 HZ, triggered by the kernel based
-    // on clock ticks.
-    let program: &mut PerfEvent = ebpf.program_mut("sikte").unwrap().try_into()?;
+
+    // // This will raise scheduled events on each CPU at 1 HZ, triggered by the kernel based
+    // // on clock ticks.
+    // let perf_event_program: &mut PerfEvent = get_perf_events_program(&mut ebpf);
+    // perf_event_program.load()?;
+    //
+    // for cpu in online_cpus().map_err(|(_, error)| error)? {
+    //     perf_event_program.attach(
+    //         perf_event::PerfTypeId::Software,
+    //         perf_event::perf_sw_ids::PERF_COUNT_SW_CPU_CLOCK as u64,
+    //         perf_event::PerfEventScope::AllProcessesOneCpu { cpu },
+    //         perf_event::SamplePolicy::Frequency(1),
+    //         true,
+    //     )?;
+    // }
+
+    let program: &mut TracePoint = get_tracepoints_program(&mut ebpf);
     program.load()?;
-    for cpu in online_cpus().map_err(|(_, error)| error)? {
-        program.attach(
-            perf_event::PerfTypeId::Software,
-            perf_event::perf_sw_ids::PERF_COUNT_SW_CPU_CLOCK as u64,
-            perf_event::PerfEventScope::AllProcessesOneCpu { cpu },
-            perf_event::SamplePolicy::Frequency(1),
-            true,
-        )?;
-    }
+    program.attach("syscalls", "sys_enter_read")?;
 
     let ctrl_c = signal::ctrl_c();
     println!("Waiting for Ctrl-C...");
