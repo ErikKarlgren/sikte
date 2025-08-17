e18e926293b575e31f1a85c363d0cd1cdc8e08d4
Erik Karlgren Domercq <erik.karldom@gmail.com>
2025-08-17 21:08:51 +0200

    refactor(syscall): ensure SyscallData has an alignment of 8
    This ensures it can be safely used with kernel ring buffers

--- sikte-common/src/raw_tracepoints/syscalls.rs
+++ sikte-common/src/raw_tracepoints/syscalls.rs
@@ -2,13 +2,15 @@
 /// userpace. It corresponds to libc's definition, but this compiles for me.
 pub type pid_t = i32;
 
+#[repr(C, align(8))]
 #[derive(Copy, Clone)]
 pub enum SyscallState {
     AtEnter { syscall_id: i64 },
     AtExit { syscall_ret: i64 },
 }
 
-#[repr(C)]
+/// Syscall Data. Aligned to 8 for use with kernel ring buffers.
+#[repr(C, align(8))]
 pub struct SyscallData {
     pub timestamp: u64,
     pub tgid: u32,
