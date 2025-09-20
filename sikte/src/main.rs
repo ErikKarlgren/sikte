e208ad0f08beceb5308aaa5e202ae783d6534ef9
Erik Karlgren Domercq <erik.karldom@gmail.com>
2025-09-21 01:07:06 +0200

    refactor(main): bump memlock rlimit in a separate function

--- sikte/src/main.rs
+++ sikte/src/main.rs
@@ -38,17 +38,7 @@ async fn main() -> anyhow::Result<()> {
     }
 
     env_logger::init();
-
-    // Bump the memlock rlimit. This is needed for older kernels that don't use the
-    // new memcg based accounting, see https://lwn.net/Articles/837122/
-    let rlim = libc::rlimit {
-        rlim_cur: libc::RLIM_INFINITY,
-        rlim_max: libc::RLIM_INFINITY,
-    };
-    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
-    if ret != 0 {
-        debug!("remove limit on locked memory failed, ret is: {ret}");
-    }
+    bump_memlock_rlimit();
 
     let mut ebpf = SikteEbpf::load()?;
     if let Err(e) = ebpf.init_logger() {
@@ -83,4 +73,17 @@ async fn main() -> anyhow::Result<()> {
     // });
 
     Ok(())
+}
+
+/// Bump the memlock rlimit. This is needed for older kernels that don't use the
+/// new memcg based accounting, see https://lwn.net/Articles/837122/
+fn bump_memlock_rlimit() {
+    let rlim = libc::rlimit {
+        rlim_cur: libc::RLIM_INFINITY,
+        rlim_max: libc::RLIM_INFINITY,
+    };
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+    if ret != 0 {
+        debug!("remove limit on locked memory failed, ret is: {ret}");
+    }
 }
