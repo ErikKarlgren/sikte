e6c015346212fc1722b5ba1e86e00b9cee2eebb4
Erik Karlgren Domercq <erik.karldom@gmail.com>
2025-09-29 00:06:17 +0200

    feat(shell): show simple summary when program finishes

--- sikte/src/subscribers/shell.rs
+++ sikte/src/subscribers/shell.rs
@@ -10,13 +10,22 @@ use crate::publishers::syscalls::to_syscall_name;
 pub struct ShellSubscriber {
     /// Match a thread to its last registered sys_enter event
     thr_to_last_sys_enter: HashMap<pid_t, SyscallData>,
+    /// Total time spent on syscalls in us
+    total_syscalls_time: f64,
 }
 
 impl ShellSubscriber {
     pub fn new() -> ShellSubscriber {
         ShellSubscriber {
             thr_to_last_sys_enter: HashMap::new(),
+            total_syscalls_time: 0f64,
         }
+    }
+}
+
+impl ShellSubscriber {
+    fn show_summary(&self) {
+        println!("Spent time on syscalls: {} us", self.total_syscalls_time);
     }
 }
 
@@ -35,21 +44,33 @@ impl EventSubscriber for ShellSubscriber {
         } = *syscall_data;
 
         match state {
-            SyscallState::AtEnter { syscall_id } => {
-                let syscall_name = to_syscall_name(syscall_id).unwrap_or("UNKNOWN");
-                println!("({pid}/{tid}) Start \"{syscall_name}\"");
-
+            SyscallState::AtEnter { .. } => {
                 self.thr_to_last_sys_enter.insert(tid, *syscall_data);
             }
             SyscallState::AtExit { .. } => match self.thr_to_last_sys_enter.remove(&tid) {
                 Some(last_data) => {
-                    let time = timestamp - last_data.timestamp;
-                    println!("({pid}/{tid}) Finished in {time} ns");
+                    if let SyscallState::AtEnter { syscall_id } = last_data.state {
+                        let syscall_name = to_syscall_name(syscall_id).unwrap_or("UNKNOWN");
+                        let time_ns = timestamp - last_data.timestamp;
+                        let time_us = time_ns as f64 / 1000f64;
+                        println!("({pid}/{tid}) {syscall_name} (took {time_us} us)");
+                        self.total_syscalls_time += time_us;
+                    } else {
+                        unreachable!(
+                            "only SyscallState::AtEnter can be stored in self.thr_to_last_sys_enter"
+                        );
+                    }
                 }
                 None => {
-                    println!("({pid}/{tid}) Finished in ??? ns");
+                    println!("({pid}/{tid}) ??? (took ??? us)");
                 }
             },
         }
+    }
+}
+
+impl Drop for ShellSubscriber {
+    fn drop(&mut self) {
+        self.show_summary();
     }
 }
