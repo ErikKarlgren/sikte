11688b23b41d71d2c5154f62d60815f3390f2ac4
Erik Karlgren Domercq <erik.karldom@gmail.com>
2025-10-03 00:35:10 +0200

    docs(pid allowlist): add docs

--- sikte-ebpf/src/common/pid_allowlist.rs
+++ sikte-ebpf/src/common/pid_allowlist.rs
@@ -1,13 +1,18 @@
-use aya_ebpf::{macros::map, maps::HashMap};
+use aya_ebpf::{bindings::BPF_F_NO_PREALLOC, macros::map, maps::HashMap};
 use sikte_common::{
     generic_types::Unused,
     raw_tracepoints::syscalls::{NUM_ALLOWED_PIDS, PidT},
 };
 
 #[map]
-/// PID or TGID allowlist. No processes outside this map will be considered
-pub static PID_ALLOW_LIST: HashMap<PidT, Unused> = HashMap::with_max_entries(NUM_ALLOWED_PIDS, 0);
+/// PID or TGID allowlist. No processes outside this map will be considered. It uses
+/// `BPF_F_NO_PREALLOC` to ensure the atomicity of insert() and remove(), and to eliminate the risk
+/// of having a removed element from the map get aliased by another element
+pub static PID_ALLOW_LIST: HashMap<PidT, Unused> =
+    HashMap::with_max_entries(NUM_ALLOWED_PIDS, BPF_F_NO_PREALLOC);
 
+/// Check if TGID is in allowlist. This operation is safe because `PID_ALLOW_LIST` is created with
+/// `BPF_F_NO_PREALLOC`, so
 pub fn is_tgid_in_allowlist(tgid: PidT) -> bool {
     unsafe { PID_ALLOW_LIST.get(&tgid).is_some() }
 }
