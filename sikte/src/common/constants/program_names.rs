bec1ac3e6361a41374ae96bb797ca2b10dcdb43d
Erik Karlgren Domercq <erik.karldom@gmail.com>
2026-01-11 19:04:20 +0100

    chore: remove unused constants

--- sikte/src/common/constants/program_names.rs
+++ sikte/src/common/constants/program_names.rs
@@ -1,5 +1,3 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pub const SIKTE_PERF_EVENTS: &str = "sikte_perf_events";
-pub const SIKTE_TRACE_POINTS: &str = "sikte_trace_points";
 pub const SIKTE_RAW_TRACE_POINT_AT_ENTER: &str = "sikte_raw_trace_point_at_enter";
 pub const SIKTE_RAW_TRACE_POINT_AT_EXIT: &str = "sikte_raw_trace_point_at_exit";
